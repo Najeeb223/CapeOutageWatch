b11bc1ca74cfbf0102b076b40ecea3bb55bfe855
najeebabdi <najeebwarsame@gmail.com>
2025-06-19 10:59:39 +0200

    json updates

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const port = process.env.PORT || 8000;
+
+//utility to help with file paths
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// Setup static folder
+// Middleware - is a function that runs between the incoming request and outgoing response
+/* app.use(express.static(path.join(__dirname, 'public')));
+ */
+/* app.get('/', (req, res) => {
+    // Give you the path to the current file you are in 
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.send('About');
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+}); */
+
+let posts =  [
+    {id: 1, title: 'Post One'},
+    {id: 2, title: 'Post Two'},
+    {id: 3, title: 'Post Three'}
+];
+app.get('/api/posts', (req, res) => {
+    // res.send - can pass a JS Object and it will get strigifyed as JSON
+    // But the specific json method is prefered - res.json
+    res.json(posts);
+
 });
 
-app.listen(8000, () => console.log('server is running on port 8000'));
+app.listen(port, () => console.log(`server is running on port ${port}`));
