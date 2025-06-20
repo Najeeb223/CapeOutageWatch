2e1745f4e6265b5aacb7c163ce91868fe776876c
najeebabdi <najeebwarsame@gmail.com>
2025-06-20 18:22:34 +0200

    fetch of id data

--- public/main.js
+++ public/main.js
@@ -2,7 +2,15 @@ const coctAlerts = async () => {
 
     const res = await fetch('https://service-alerts.cct-datascience.xyz/coct-service_alerts-current-unplanned.json');
     const data = await res.json();
-    console.log(data);
+
+    setTimeout(() => {
+
+        let output = "";
+        data.forEach((alerts, index) => {
+           output += `<li>${alerts.Id}</li>`
+        });
+        document.body.innerHTML = output;
+    }, 1000);
 }
 coctAlerts().catch(console.error);
 
--- server.js
+++ server.js
@@ -6,14 +6,12 @@ const path = require('path');
 
 const app = express();
 
+
+
 // Setup static folder
 // Middleware - is a function that runs between the incoming request and outgoing response
-/* app.use(express.static(path.join(__dirname, 'public')));
- */
- app.get('/', (req, res) => {
-    // Give you the path to the current file you are in 
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+ app.use(express.static(path.join(__dirname, 'public')));
+ 
 
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
