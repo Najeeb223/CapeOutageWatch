8b2e26933123d2281a7447c378e7c37753750abc
najeebabdi <najeebwarsame@gmail.com>
2025-06-20 15:07:05 +0200

    yaml update

--- public/main.js
+++ public/main.js
@@ -5,3 +5,4 @@ const coctAlerts = async () => {
     console.log(data);
 }
 coctAlerts().catch(console.error);
+
--- server.js
+++ server.js
@@ -16,13 +16,4 @@ const app = express();
 });
 
 
-
-const coctAlerts = async () => {
-
-    const res = await fetch('https://service-alerts.cct-datascience.xyz/coct-service_alerts-current-unplanned.json');
-    const data = await res.json();
-    console.log(data);
-}
-coctAlerts().catch(console.error);
-
 app.listen(port, () => console.log(`server is running on port ${port}`));
