8717517d90cdd989d4eb3faae6b9b152c84a43e6
najeebabdi <najeebwarsame@gmail.com>
2025-06-21 21:08:51 +0200

    html update

--- public/main.js
+++ public/main.js
@@ -5,64 +5,16 @@ const coctAlerts = async () => {
 
     setTimeout(() => {
 
-        let titleOutput = "";
+        let output = "";
         data.forEach((alerts, index) => {
             if(alerts.service_area === "Water & Sanitation"){
-                titleOutput += `<li>${alerts.title}</li>`;
+                output += `${alerts.title}, ${alerts.area}
+                `;
+
         }
         });
-        document.getElementById("title-text").innerHTML = titleOutput;
+        document.getElementById("title-text").innerHTML = output;
     }, 1000);
-
-    setTimeout(() => {
-
-        let areaOutput = "";
-        data.forEach((alerts, index) => {
-            if(alerts.service_area === "Water & Sanitation"){
-                areaOutput += `<li>${alerts.area}</li>`;
-        }
-        });
-        document.getElementById("area-text").innerHTML = areaOutput;
-    }, 1000);
-
-    setTimeout(() => {
-
-        let locationOutput = "";
-        data.forEach((alerts, index) => {
-            if(alerts.service_area === "Water & Sanitation"){
-                locationOutput += `<li>${alerts.location}</li>`;
-        }
-        });
-        document.getElementById("location-text").innerHTML = locationOutput;
-    }, 1000);
-
-    setTimeout(() => {
-
-        let startTimestampOutput = "";
-        data.forEach((alerts, index) => {
-            if(alerts.service_area === "Water & Sanitation"){
-                startTimestampOutput += `<li>${alerts.start_timestamp}</li>`;
-        }
-        });
-        document.getElementById("start-timestamp-text").innerHTML = startTimestampOutput;
-    }, 1000);
-
-    setTimeout(() => {
-
-        let forecastEndTimestampOutput = "";
-        data.forEach((alerts, index) => {
-            if(alerts.service_area === "Water & Sanitation"){
-                forecastEndTimestampOutput += `<li>${alerts.forecast_end_timestamp}</li>`;
-        }
-        });
-        document.getElementById("forecast-end-timestamp-text").innerHTML = forecastEndTimestampOutput;
-    }, 1000);
-
-    
-
-    
-
-
     
 }
 coctAlerts().catch(console.error);
