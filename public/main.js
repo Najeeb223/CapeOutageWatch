82cd4713639c0868efb4bfa32b36237159aafb83
najeebabdi <najeebwarsame@gmail.com>
2025-06-21 16:58:50 +0200

    DOM data update

--- public/main.js
+++ public/main.js
@@ -5,18 +5,65 @@ const coctAlerts = async () => {
 
     setTimeout(() => {
 
-        let output = "";
+        let titleOutput = "";
         data.forEach((alerts, index) => {
             if(alerts.service_area === "Water & Sanitation"){
-           output += `<li>${alerts.service_area}</li>
-                      <li>${alerts.title}</li>
-                      <li>${alerts.area}</li>
-                      <li>${alerts.location}</li>
-                      <li>${alerts.publish_date}</li>`;
+                titleOutput += `<li>${alerts.title}</li>`;
         }
         });
-        document.body.innerHTML = output;
+        document.getElementById("title-text").innerHTML = titleOutput;
     }, 1000);
+
+    setTimeout(() => {
+
+        let areaOutput = "";
+        data.forEach((alerts, index) => {
+            if(alerts.service_area === "Water & Sanitation"){
+                areaOutput += `<li>${alerts.area}</li>`;
+        }
+        });
+        document.getElementById("area-text").innerHTML = areaOutput;
+    }, 1000);
+
+    setTimeout(() => {
+
+        let locationOutput = "";
+        data.forEach((alerts, index) => {
+            if(alerts.service_area === "Water & Sanitation"){
+                locationOutput += `<li>${alerts.location}</li>`;
+        }
+        });
+        document.getElementById("location-text").innerHTML = locationOutput;
+    }, 1000);
+
+    setTimeout(() => {
+
+        let startTimestampOutput = "";
+        data.forEach((alerts, index) => {
+            if(alerts.service_area === "Water & Sanitation"){
+                startTimestampOutput += `<li>${alerts.start_timestamp}</li>`;
+        }
+        });
+        document.getElementById("start-timestamp-text").innerHTML = startTimestampOutput;
+    }, 1000);
+
+    setTimeout(() => {
+
+        let forecastEndTimestampOutput = "";
+        data.forEach((alerts, index) => {
+            if(alerts.service_area === "Water & Sanitation"){
+                forecastEndTimestampOutput += `<li>${alerts.forecast_end_timestamp}</li>`;
+        }
+        });
+        document.getElementById("forecast-end-timestamp-text").innerHTML = forecastEndTimestampOutput;
+    }, 1000);
+
+    
+
+    
+
+
+    
 }
 coctAlerts().catch(console.error);
 
