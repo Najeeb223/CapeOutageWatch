9424b02fd57723f750b04ab39e48681d71102b93
najeebabdi <najeebwarsame@gmail.com>
2025-06-22 17:31:29 +0200

    html update

--- public/main.js
+++ public/main.js
@@ -3,18 +3,25 @@ const coctAlerts = async () => {
     const res = await fetch('https://service-alerts.cct-datascience.xyz/coct-service_alerts-current-planned.json');
     const data = await res.json();
 
-    setTimeout(() => {
 
-        let output = "";
+        let output = `<h3>Title</h3>
+                      <h3>Area</h3>
+                      <h3>Location</h3>
+                      <h3>Start</h3>
+                      <h3>Forecasted End</h3>
+                    `;
         data.forEach((alerts, index) => {
             if(alerts.service_area === "Water & Sanitation"){
-                output += `${alerts.title}, ${alerts.area}
+                output += `<p>${alerts.title}</p>
+                           <p>${alerts.area}</p> 
+                           <p>${alerts.location}</p> 
+                           <p>${alerts.start_timestamp}</p> 
+                           <p>${alerts.forecast_end_timestamp}</p>    
                 `;
-
         }
         });
-        document.getElementById("title-text").innerHTML = output;
-    }, 1000);
+        document.querySelector(".planned-alert-card").innerHTML = output;
+ 
     
 }
 coctAlerts().catch(console.error);
