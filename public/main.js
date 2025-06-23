6b5f581fffefc596c94ced47c6cfe7fb2cf4c016
najeebabdi <najeebwarsame@gmail.com>
2025-06-23 18:24:02 +0200

    CSS updates

--- public/main.js
+++ public/main.js
@@ -4,31 +4,30 @@ const coctAlerts = async () => {
     const data = await res.json();
 
 
-        let output = ""; 
         data.forEach((alerts, index) => {
             if(alerts.service_area === "Water & Sanitation"){
-                output += `<h3>Title</h3>
-                           <p>${alerts.title}</p>
 
-                           <h3>Area</h3>
-                           <p>${alerts.area}</p>
+                let newElement = document.createElement("div");
+                newElement.innerHTML = ` <h3>Title</h3>
+                                        <p>${alerts.title}</p>
 
-                           <h3>Location</h3>
-                           <p>${alerts.location}</p> 
+                                        <h3>Area</h3>
+                                        <p>${alerts.area}</p>
 
-                           <h3>Start Time</h3>                  
-                           <p>${alerts.start_timestamp}</p> 
+                                        <h3>Location</h3>
+                                        <p>${alerts.location}</p> 
 
-                           <h3>Forecasted End</h3>
-                           <p>${alerts.forecast_end_timestamp}</p>    
-                `;
+                                        <h3>Start Time</h3>                  
+                                        <p>${alerts.start_timestamp}</p> 
+
+                                        <h3>Forecasted End</h3>
+                                        <p>${alerts.forecast_end_timestamp}</p> 
+    `;
+                document.querySelector(".planned-alerts-layout").appendChild(newElement);
+   
         }
         });
-        for(let output = 0; output < data; output++){
-            document.querySelector(".planned-alerts-layout").innerHTML = output;
-        }
-        
- 
+
     
 }
 coctAlerts().catch(console.error);
