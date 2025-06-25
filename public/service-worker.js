240c0bfa2d72d43ecaa71e735f79259fe6191992
najeebabdi <najeebwarsame@gmail.com>
2025-06-25 21:03:55 +0200

    service worker update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,22 @@
 
 
 self.addEventListener("install", e => {
-    console.log("Install!");
+    console.log("Install CapeOutageWatch!");
 })
+
+/*  Caching of service worker
+
+self.addEventListener("install", e => {
+    e.waitUntil(caches.open("static").then(cache => {
+
+        return cache.addAll(directory and files in directory as well as images)
+        })
+    })
+*/
+
+self.addEventListener("fetch", e => {
+    console.log(`Intercepting fetch request for: ${e.request.url}`);
+} )
 
 
 
