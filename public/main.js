e2b82ddcd4afe2ea8c0f6a651f7a62f8f7d4b9d3
najeebabdi <najeebwarsame@gmail.com>
2025-06-26 10:46:03 +0200

    main.js update

--- public/main.js
+++ public/main.js
@@ -1,13 +1,4 @@
-if("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("service-worker.js").then(registration => {
-            console.log("SW Registered!");
-            console.log(registration);
-     
-        }).catch(error => {
-            console.log("SW Registration failed");
-        })
-    }
-    
+
 function formatCapeToDate(timestamp) {
         if (!timestamp) {
             return "Not specified";
