25a91aee17632f49ab72ffccec3caaf5cfd16def
najeebabdi <najeebwarsame@gmail.com>
2025-06-27 20:51:07 +0200

    service worker update

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,11 +13,11 @@ self.addEventListener("install", e => {
         })
     })
 */
-
+/*
 self.addEventListener("fetch", e => {
     console.log(`Intercepting fetch request for: ${e.request.url}`);
 } )
-
+*/
 
 
 
