549d6e81506d96c2952607fe015258f16743434e
najeebabdi <najeebwarsame@gmail.com>
2025-07-29 22:11:30 +0200

    install prompt update

--- main.js
+++ main.js
@@ -136,6 +136,31 @@ const main = async () => {
 main().catch(console.error);
 
 
+const installApp = () => {
+
+    let installPrompt = null;
+    const installButton = document.getElementById("install-btn");
+    
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault();
+      installPrompt = event;
+      installButton.removeAttribute("hidden");
+    });
+
+    installButton.addEventListener("click", async () => {
+        if (!installPrompt) {
+          return;
+        }
+        const result = await installPrompt.prompt();
+        console.log(`Install prompt was: ${result.outcome}`);
+        installPrompt = null;
+        installButton.setAttribute("hidden", "");
+      });
+    
+
+}
+installApp();
+
 /*
 const searchArea = async () => {
 
